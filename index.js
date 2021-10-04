7976a139668ae81ef1cf0166773330242cb70ffc
QuentinChevaux <quentin.chevaux@gmail.com>
2021-10-04 15:34:48 +0200

    Exercice Compteur

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-let date_finale = new Date("Jul 20, 2022 8:00:00").getTime();
+let date_finale = new Date("Jul 20, 2022 17:00:00").getTime();
 
 let every_second = setInterval(function() {
 
-    let date_actuelle = new Date().getTime();
+    let date_actuelle = new Date();
 
     let distance = date_finale - date_actuelle;
-
-    console.log(distance);
 
     let jours = Math.floor(distance / (1000 * 60 * 60 * 24));
     let heures = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let secondes = Math.floor((distance % (1000 * 60)) / 1000);
+    let secondes = Math.floor((distance % (1000 * 60)) / 1000); 
 
     document.getElementById("jour").innerHTML = jours + " jours";
 
@@ -21,4 +19,4 @@ let every_second = setInterval(function() {
 
     document.getElementById("secondes").innerHTML = secondes + " Secondes";
 
-}, 1000);
+}, 500);
